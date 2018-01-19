9d405f13a9a126a737f8023b6415ad2bf71b7188
mehmetfyuce <mehmet.yuce@bitekanalytics.com>
2018-01-19 14:41:11 +0300

    -added helmet library for server -Each widget will have row/colum/widget indexes now -Cannot resolve module 'react/lib/ReactComponentTreeHook'  on npm run dev  (react version made te same with react-dom) -ability to receive props from layout (previously only from widgets. This way, single widget list and multiple layouts can be constructed).

--- test/components/Widgets.spec.js
+++ test/components/Widgets.spec.js
@@ -32,8 +32,8 @@ describe('<Widgets />', () => {
     const rowIndex = 6;
     const widgetIndex = 0;
     const editable = false;
-    const frame = () => {};
-    const onRemove = () => {};
+    const frame = () => { };
+    const onRemove = () => { };
 
     const component = shallow(
       <Widgets
@@ -59,6 +59,76 @@ describe('<Widgets />', () => {
     expect(component.find(WidgetFrame).at(0).prop('onRemove')).to.equal(onRemove);
   });
 
+
+  it('Should include row/column/widget index', () => {
+    const widgets = [{ key: 'HelloWorld' }];
+    const widgetTypes = {
+      HelloWorld: {
+        type: TestComponent,
+        title: 'Sample Hello World App',
+      },
+    };
+
+    const layout = {};
+    const columnIndex = 5;
+    const rowIndex = 6;
+    const widgetIndex = 0;
+    const editable = false;
+    const frame = () => { };
+    const onRemove = () => { };
+
+    const component = shallow(
+      <Widgets
+        widgets={widgets}
+        widgetTypes={widgetTypes}
+        layout={layout}
+        columnIndex={columnIndex}
+        rowIndex={rowIndex}
+        widgetIndex={widgetIndex}
+        editable={editable}
+        frameComponent={frame}
+        onRemove={onRemove}
+      />
+    );
+
+    expect(component.find(TestComponent).at(0).prop('rowIndex')).to.equal(rowIndex);
+    expect(component.find(TestComponent).at(0).prop('columnIndex')).to.equal(columnIndex);
+    expect(component.find(TestComponent).at(0).prop('widgetIndex')).to.equal(widgetIndex);
+  });
+
+  it('Layout may include props', () => {
+    const widgets = [{ key: 'HelloWorld', props: { tryme: 'tryme' } }];
+    const widgetTypes = {
+      HelloWorld: {
+        type: TestComponent,
+        title: 'Sample Hello World App',
+      },
+    }; 
+    
+    const layout = {};
+    const columnIndex = 5;
+    const rowIndex = 6;
+    const widgetIndex = 0;
+    const editable = false;
+    const frame = () => { };
+    const onRemove = () => { };
+
+    const component = shallow(
+      <Widgets
+        widgets={widgets}
+        widgetTypes={widgetTypes}
+        layout={layout}
+        columnIndex={columnIndex}
+        rowIndex={rowIndex}
+        widgetIndex={widgetIndex}
+        editable={editable}
+        frameComponent={frame}
+        onRemove={onRemove}
+      />
+    );
+
+    expect(component.find(TestComponent).at(0).prop('tryme')).to.equal('tryme');
+  });
   it('Frame should have the actual widget as children', () => {
     const widgets = [{ key: 'HelloWorld' }];
     const widgetTypes = {
