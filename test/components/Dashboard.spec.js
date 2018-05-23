9935be4459dc5b231288ac962ab43a97669e8738
sebastien perin <s.perin@evolucare.com>
2018-05-23 12:16:02 +0200

    Added test case for DashboardWithoutDndContext in Dashboard.spec.js

--- test/components/Dashboard.spec.js
+++ test/components/Dashboard.spec.js
@@ -1,12 +1,19 @@
 import { expect } from 'chai';
 import React from 'react';
 import { mount } from 'enzyme';
-import Dashbaord from '../../lib/components/Dashboard';
+import { default as Dashboard, DashboardWithoutDndContext as DashboardWithoutDndContext } from '../../lib/components/Dashboard';
 import LayoutRenderer from '../../lib/components/LayoutRenderer';
 
 describe('<Dashboard />', () => {
   it('Should have a  <LayoutRenderer />', () => {
-    const component = mount(<Dashbaord />);
+    const component = mount(<Dashboard />);
+    expect(component.find(LayoutRenderer)).to.have.length(1);
+  });
+});
+
+describe('<DashboardWithoutDndContext />', () => {
+  it('Should have a  <LayoutRenderer />', () => {
+    const component = mount(<DashboardWithoutDndContext />);
     expect(component.find(LayoutRenderer)).to.have.length(1);
   });
 });
